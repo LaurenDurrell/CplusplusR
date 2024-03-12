0a6fcb983d0c20f920605d1da8f983b487d297cf
Durrell <durrell@lancaster.ac.uk>
2024-03-12 19:51:06 +0000

    Create data_checking_functions.R

---
+++ R/data_checking_functions.R
@@ -0,0 +1,19 @@
+#random permutations of the data.frame rows 
+shuffle_function <- function(df) { 
+  shuffled_df<- df[ ,sample(1:ncol(df))] 
+  return (shuffled_df)
+} 
+
+#input data checks
+check_data_inputs <- function(df1, df2) {
+  if(sum(dim(df1))==sum(dim(df2))) {print("preference tables the same size")}
+  unique_cols_df1 <- sapply(df1, function(col) length(unique(col)))
+  if (all(unique_cols_df1 == sapply(df1, length))) {
+    print("No duplicate entries within each column of preference table 1")}
+  if(length(unique(names(df1)))==length(names(df1))) {print("no duplicates names in preference table names")}
+  if(class(df1)=="data.frame"|class(df1)=="list") {print("df1 correct type")}
+  if(class(df2)=="data.frame"|class(df2)=="list") {print("df2 correct type")} 
+  else(print("errors in input data"))
+}
+
+
